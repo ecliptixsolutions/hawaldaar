6f3cdeabf3d7796c4bb3d057aead48158c4dae48
parthram7071 <parthram7075@gmail.com>
2025-09-05 20:34:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function createFrames() {
+  var frameCountElement = document.getElementById("number-of-frames-input");
+
+  if (frameCountElement != null) {
+    var webAddressInput = document.getElementById("web-address-input");
+    var webAddress = "";
+
+    if (webAddressInput != null) {
+      webAddress = webAddressInput.value;
+    }
+
+    var frameCount = parseInt(frameCountElement.value);
+    var framesContainer = document.getElementById("frames-container");
+    framesContainer.innerHTML = "";
+
+    for (var i = 0; i < frameCount; i++) {
+      var frameDiv = document.createElement("iframe");
+      frameDiv.src = webAddress;
+      frameDiv.classList.add("content-frame");
+      framesContainer.appendChild(frameDiv);
+    }
+  }
+}
